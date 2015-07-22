d074b8c5f9d11e572ac7e953efdab339273408f2
Urs Thuermann <urs@isnogud.escape.de>
2015-07-22 23:02:24 +0200

    Eliminate duplicate solutions with respect to rotating/mirroring
    Add option -u to activate duplicates elimination.

--- queens.c
+++ queens.c
@@ -1,5 +1,5 @@
 /*
- * $Id: queens.c,v 1.9 2015/07/22 20:58:43 urs Exp $
+ * $Id: queens.c,v 1.10 2015/07/22 21:02:24 urs Exp $
  *
  * Try to place n queens on a n by n chess board so that
  * no two queens attack each other.
@@ -7,32 +7,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-static void solve(int *pos, int dim, int col);
+static void solve(int *pos, int dim, int col, int uniq);
 static int  attacked(const int *pos, int col, int row);
+static int  checkdup(int *pos, int dim);
+static void mirror(int *pos, int dim);
+static void rotate(int *pos, int dim);
+static int  cmp(const int *a, const int *b, int dim);
 static void found(const int *pos, int dim);
 
 int main(int argc, char **argv)
 {
-	int dim = 8;
+	int dim  = 8;
+	int uniq = 0;
 	int *pos;
 
+	if (argc > 1 && strcmp(*++argv, "-u") == 0)
+		--argc, ++argv, uniq = 1;
 	if (argc > 1)
-		dim = atoi(*++argv);
+		dim = atoi(*argv);
 
 	if (!(pos = malloc(dim * sizeof(int)))) {
 		fputs("Insufficient memory.\n", stderr);
 		exit(1);
 	}
 
-	solve(pos, dim, 0);
+	solve(pos, dim, 0, uniq);
 
 	free(pos);
 
 	return 0;
 }
 
-static void solve(int *pos, int dim, int col)
+static void solve(int *pos, int dim, int col, int uniq)
 {
 	int row;
 
@@ -42,7 +50,9 @@ static void solve(int *pos, int dim, int col)
 
 		pos[col] = row;
 		if (col < dim - 1)
-			solve(pos, dim, col + 1);
+			solve(pos, dim, col + 1, uniq);
+		else if (uniq && checkdup(pos, dim))
+			putchar('\n');
 		else
 			found(pos, dim);
 	}
@@ -56,6 +66,75 @@ static int attacked(const int *pos, int col, int row)
 		if (row == pos[c] || abs(row - pos[c]) == col - c)
 			return 1;
 	return 0;
+}
+
+static int checkdup(int *pos, int dim)
+{
+	int min[dim], tmp[dim];
+	int flag = 0;
+	int i;
+
+	memcpy(min, pos, sizeof(min));
+	memcpy(tmp, min, sizeof(tmp));
+	for (i = 0; i < 4; i++) {
+		if (cmp(tmp, min, dim) < 0) {
+			memcpy(min, tmp, dim * sizeof(int));
+			flag = 1;
+		}
+		rotate(tmp, dim);
+	}
+	mirror(tmp, dim);
+	for (i = 0; i < 4; i++) {
+		if (cmp(tmp, min, dim) < 0) {
+			memcpy(min, tmp, dim * sizeof(int));
+			flag = 1;
+		}
+		rotate(tmp, dim);
+	}
+	return flag;
+}
+
+static void mirror(int *pos, int dim)
+{
+	int c;
+
+	for (c = 0; c < dim; c++)
+		pos[c] = dim - 1 - pos[c];
+}
+
+static void rotate(int *pos, int dim)
+{
+	int tmp[dim];
+	int c, cc;
+
+	for (c = 0; c < dim; c++) {
+		for (cc = 0; cc < dim; cc++)
+			if (pos[cc] == dim - 1 - c) {
+				tmp[c] = cc;
+				break;
+			}
+	}
+	memcpy(pos, tmp, sizeof(tmp));
+}
+
+static int cmp(const int *a, const int *b, int dim)
+{
+	int c;
+	int res;
+
+	for (c = 0; c < dim; c++)
+		if ((res = a[c] - b[c]) != 0)
+			break;
+
+	printf("cmp:");
+	for (c = 0; c < dim; c++)
+		printf(" %c%d", c + 'a', a[c] + 1);
+	putchar(',');
+	for (c = 0; c < dim; c++)
+		printf(" %c%d", c + 'a', b[c] + 1);
+	printf(": %d\n", res);
+
+	return res;
 }
 
 static void found(const int *pos, int dim)
